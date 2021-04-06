cb12c4fc5cd059d739bd0c8aefcc67c1a727a08a
Krllka <k.r.llaleks25@gmail.com>
2021-04-07 00:02:35 +0400

    Init project

---
+++ backend/src/main/java/backend/backapp/BackappApplication.java
@@ -0,0 +1,13 @@
+package backend.backapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackappApplication.class, args);
+    }
+
+}
