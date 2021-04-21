aec07ee6cb4c2c3024bc848bf5ff2844a6e57372
Krllka <k.r.llaleks25@gmail.com>
2021-04-21 23:42:57 +0400

    added simple uploading file controller

--- src/main/java/backend/controllers/Impl/ProductsController.java
+++ src/main/java/backend/controllers/Impl/ProductsController.java
@@ -1,10 +1,17 @@
 package backend.controllers.Impl;
 
 import backend.model.ProductsData;
-import backend.services.Impl.AccountDataServiceImpl;
 import backend.services.Impl.ProductsDataServiceImpl;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 @CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200", "http://localhost:8081" })
 @RestController
@@ -14,8 +21,26 @@ public class ProductsController extends AbstractController<ProductsData, Product
     public ProductsController(ProductsDataServiceImpl Service) {
         super(Service);
     }
+    @Value("${upload.path}")
+    String upDir;
 
+    @RequestMapping(value = "/files/{file_name:.+}", method = RequestMethod.GET)
+    public void getFile(@PathVariable("file_name") String fileName, HttpServletResponse response) {
+        // Прежде всего стоит проверить, если необходимо, авторизован ли пользователь и имеет достаточно прав на скачивание файла. Если нет, то выбрасываем здесь Exception
 
+        //Авторизованные пользователи смогут скачать файл
+        Path file = Paths.get(upDir, fileName);
+        if (Files.exists(file)){
+            response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+            response.setContentType("application/vnd.ms-excel");
+
+            try {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException e) {
+                            }
+        }
+    }
 
 
 }
