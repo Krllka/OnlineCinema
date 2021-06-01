f5521cae4fa9a9192a473cbecd6b3df3b7e71c3f
Aleksandr Sukhov <sukhov.aa@bk.ru>
2021-06-01 21:58:01 +0400

    router and TheHeader component ready

---
+++ ui/src/components/TheHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <router-link to="/">Онлайн-кинотеатр</router-link>
+      <nav class="menu">
+        <ul class="menu__list">
+          <li class="menu__item">
+            <router-link to="/">Каталог</router-link>
+          </li>
+          <li class="menu__item">
+            <router-link to="/library">Библиотека</router-link>
+          </li>
+          <li class="menu__item">
+            <router-link to="/orders">Заказы</router-link>
+          </li>
+          <li class="menu__item">
+            <router-link to="/profile">Мой профиль</router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.header {
+  padding: 15px 0;
+  border-bottom: 1px solid black;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    & > a {
+      cursor: pointer;
+      font-size: 36px;
+      font-weight: 700;
+      transition: 0.3s all;
+      &:hover {
+        color: rgba(0, 0, 0, 0.5);
+      }
+    }
+  }
+  .menu {
+    &__list {
+      display: flex;
+      justify-content: space-between;
+      width: 25vw;
+    }
+    &__item a {
+      font-size: 17px;
+      transition: 0.3s all;
+      &:hover {
+        color: rgba(0, 0, 0, 0.5);
+        border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+      }
+    }
+  }
+}
+</style>
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,23 +1,38 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import PageCatalog from "@/views/PageCatalog";
+import PageUserCabinet from "@/views/PageUserCabinet";
+import PageLibrary from "@/views/PageLibrary";
+import PageOrders from "@/views/PageOrders";
+import PageAdmin from "@/views/PageAdmin";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Catalog",
+    component: PageCatalog,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/profile",
+    name: "Profile",
+    component: PageUserCabinet,
+  },
+  {
+    path: "/library",
+    name: "Library",
+    component: PageLibrary,
+  },
+  {
+    path: "/orders",
+    name: "Orders",
+    component: PageOrders,
+  },
+  {
+    path: "/admin",
+    name: "Admin Panel",
+    component: PageAdmin,
   },
 ];
 
