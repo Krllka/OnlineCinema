336833dda73d2fd3375e4c6ff328831312d1600e
Aleksandr Sukhov <sukhov.aa@bk.ru>
2021-06-06 23:27:38 +0400

    Added message for AppDataTable when FilteredList is empty

--- ui/src/components/AppDataTable.vue
+++ ui/src/components/AppDataTable.vue
@@ -12,21 +12,30 @@
       </tr>
     </thead>
     <tbody class="table__body">
-      <tr
-        v-for="(item, index) in filteredList"
-        :key="index"
-        class="table__row data"
-      >
-        <td
-          v-for="(column, index) in dataHeaders"
+      <template v-if="filteredList.length">
+        <tr
+          v-for="(item, index) in filteredList"
           :key="index"
-          class="table__cell"
+          class="table__row data"
         >
-          <slot :name="column.key" :movie="item" :index="index">
-            {{ item[column.key] }}
-          </slot>
-        </td>
-      </tr>
+          <td
+            v-for="(column, index) in dataHeaders"
+            :key="index"
+            class="table__cell"
+          >
+            <slot :name="column.key" :movie="item" :index="index">
+              {{ item[column.key] }}
+            </slot>
+          </td>
+        </tr>
+      </template>
+      <template v-else>
+        <tr class="table__row">
+          <td class="table__cell" :colspan="dataHeaders.length">
+            <div class="empty">Нет совпадений.</div>
+          </td>
+        </tr>
+      </template>
     </tbody>
   </table>
 </template>
@@ -84,6 +93,12 @@ export default {
     &:hover {
       background-color: rgba(0, 0, 0, 0.1);
     }
+  }
+  .empty {
+    font-size: 16px;
+    font-weight: 500;
+    text-align: center;
+    margin: 25px 0;
   }
 }
 </style>
