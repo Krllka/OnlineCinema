43dd0bac692052e27b7e00ed55ad3594d32366f8
Krllka <k.r.llaleks25@gmail.com>
2021-06-07 18:36:51 +0400

    added genres list to prodcts

--- src/main/java/backend/DAO/Impl/ProdGenreDAOimpl.java
+++ src/main/java/backend/DAO/Impl/ProdGenreDAOimpl.java
@@ -2,6 +2,7 @@ package backend.DAO.Impl;
 
 import backend.model.ProductsGenresData;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Repository;
@@ -24,5 +25,22 @@ public class ProdGenreDAOimpl extends AbstractDAO<ProductsGenresData>{
     public ProductsGenresData getById(String id) {
         Session session = sessionFactory.getCurrentSession();
         return session.get(ProductsGenresData.class ,id);
+    }
+    @Override
+    public void add(ProductsGenresData film) {
+        Session session = sessionFactory.getCurrentSession();
+        String currID = film.getProd().getId();
+        Query<ProductsGenresData> query  = session.createQuery("from ProductsGenresData o where o.prod.id = :currID");
+        query.setParameter("currID", currID);
+        List<ProductsGenresData> list = query.list();
+        for (ProductsGenresData item: list) {
+            if(item.equals(film)) {
+                System.out.println("-----------------");
+                System.out.println(item.equals(film));
+                System.out.println("-----------------");
+                return;
+            }
+        }
+        session.save(film);
     }
 }
