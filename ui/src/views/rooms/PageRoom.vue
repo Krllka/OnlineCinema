87d4425d525af06c0dcc054d96759bc4e6a59816
Aleksandr Sukhov <sukhov.aa@bk.ru>
2021-06-13 13:54:22 +0400

    check sending videofile

--- ui/src/views/rooms/PageRoom.vue
+++ ui/src/views/rooms/PageRoom.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="container">
-    <h2 class="title">{{ roomData.name }}</h2>
-    <div>{{ movieData.name }}</div>
+    <h2 class="room__title">{{ roomData.name }}</h2>
+    <div class="movie__title">{{ movieData.name }}</div>
+    <div class="movie__video">
+      <video
+        v-if="!videoError"
+        controls
+        controlsList="nodownload"
+        width="640"
+        height="480"
+        @error="videoError = !videoError"
+      >
+        <source
+          type="video/mp4"
+          :src="`http://localhost:8080/products/files/${videoData.filePath}`"
+        />
+      </video>
+      <div v-else class="message">Фильм не найден.</div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +28,8 @@ export default {
     return {
       roomData: {},
       movieData: {},
+      videoData: {},
+      videoError: false,
     };
   },
   created() {
@@ -25,15 +43,41 @@ export default {
   },
   methods: {
     getMovie(id) {
+      const movieURL = this.axios.get(`http://localhost:8081/products/${id}`);
+      const videoURL = this.axios.get(`http://localhost:8081/mainFiles/${id}`);
       this.axios
-        .get(`http://localhost:8081/products/${id}`)
-        .then((response) => {
-          this.movieData = response.data;
-        })
-        .catch((error) => console.log(error));
+        .all([movieURL, videoURL])
+        .then(
+          this.axios.spread((movieRes, videoRes) => {
+            this.movieData = movieRes.data;
+            this.videoData = videoRes.data;
+          })
+        )
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.room__title {
+  margin-top: 20px;
+  text-align: center;
+}
+.movie__video {
+  video {
+    display: block;
+    max-height: 300px;
+    width: auto;
+    margin: 20px auto;
+  }
+}
+.message {
+  text-align: center;
+  margin: 25px auto;
+  font-size: 20px;
+  font-weight: 700;
+}
+</style>
